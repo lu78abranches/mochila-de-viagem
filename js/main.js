6ed66cec42524c07e5a3083d6640bee55c4cc78a
lu78abranches <luisabranches.surf@hotmail.com>
2023-05-18 00:46:47 -0300

    Finalizando a remoção de itens no localStorage

--- js/main.js
+++ js/main.js
@@ -12,15 +12,30 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
     
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
+    
+    
     const itemAltual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
+
+    if(existe) {
+        itemAltual.id = existe.id;
+
+        atualizaElemento(itemAltual);
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAltual;
+    } else{
+        itemAltual.id = itens[itens.length-1] ? (itens[itens.length-1]).id + 1 : 0; ;
+
+        criaElemento(itemAltual );
+
+        itens.push(itemAltual);
+    }
     
-    criaElemento(itemAltual );
-
-
-    itens.push(itemAltual);
+    
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -39,16 +54,38 @@ function criaElemento(item) {
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
     
+    numeroItem.dataset.id = item.id;
+
     novoItem.appendChild(numeroItem);
 
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDelete(item.id));       
+
     lista.appendChild(novoItem);
-
-    
-
-    
-
-
-    
+ 
 }
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDelete(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "X";
+
+    elementoBotao.addEventListener("click", function() { //nesse caso náo foi usado uma arrow function porque ela não carrega o "this"
+        deleteElemento(this.parentNode, id);
+    })
+
+    return elementoBotao;
+}
+
+function deleteElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
+
